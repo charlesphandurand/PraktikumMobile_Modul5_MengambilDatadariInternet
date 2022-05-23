23ed2dd7c728beadb2f3d0d9930bb1641d7df979
Charles Phandurand <charles.phandurand@gmail.com>
2022-05-24 00:02:34 +0800

    Initial commit

--- app/src/main/java/com/example/animes/ui/AnimeDetailFragment.kt
+++ app/src/main/java/com/example/animes/ui/AnimeDetailFragment.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2021 The Android Open Source Project.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.animes.ui
 
 import android.os.Bundle
@@ -23,9 +8,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.animes.databinding.FragmentAnimeDetailBinding
 
-/**
- * This Fragment shows the detailed information on a particular Anime
- */
 class AnimeDetailFragment : Fragment() {
 
     private val viewModel: AnimeViewModel by activityViewModels()
@@ -38,8 +20,6 @@ class AnimeDetailFragment : Fragment() {
         val binding = FragmentAnimeDetailBinding.inflate(inflater)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
-
-        // Inflate the layout for this fragment
         return binding.root
     }
 }
